96fd69b010e4023fd8202d4e78b9026e7f0227c2
Alan Smith <alan@randomsmiths.com>
2020-07-29 20:18:07 -0400

    Not sure this is necessary but want to save it

--- src/main/java/org/quantumquacks/plugins/MultiUserGit.java
+++ src/main/java/org/quantumquacks/plugins/MultiUserGit.java
@@ -1,34 +1,14 @@
 package org.quantumquacks.plugins;
 
-import com.intellij.openapi.actionSystem.PlatformDataKeys;
-import com.intellij.openapi.components.ProjectComponent;
-import com.intellij.psi.search.FilenameIndex;
+import com.intellij.openapi.components.Service;
+import com.intellij.openapi.project.Project;
 
-import org.jetbrains.annotations.NotNull;
 
-public class MultiUserGit implements ProjectComponent {
-    private static final String COMPONENT_NAME = "MultiUserGit";
+@Service
+public final class MultiUserGit {
+    private final Project myProject;
 
-    @Override
-    public void projectOpened() {
-    }
-
-    @Override
-    public void projectClosed() {
-    }
-
-    @Override
-    public void initComponent() {
-        System.out.println(COMPONENT_NAME + " - Initialized");
-    }
-
-    @Override
-    public void disposeComponent() {
-    }
-
-    @NotNull
-    @Override
-    public String getComponentName() {
-        return COMPONENT_NAME;
+    public MultiUserGit(Project project) {
+        myProject = project;
     }
 }
